d3f156389e46b0537d0abd7138d17d682fb6f47c
auscay <ebowusifeanyi@gmail.com>
2024-08-01 03:33:28 +0100

    Implemented rate limiter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
+const rateLimit = require('express-rate-limit')
 const { connectToMongoDB } = require('./db')
 const PORT = process.env.PORT
 
@@ -12,9 +13,19 @@ connectToMongoDB()
 
 const app = express()
 
+// Configure rate limiter
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+})
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.json())
+
+app.use(limiter)
 
 app.get("/", (req, res) => {
     res.send("Welcome To Taskly!")
